335a6b47ec6c578d4404de642d1832bd283046b2
Sam <sammorton11@yahoo.com>
2023-07-24 00:06:37 -0500

    NextUI

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {Component} from 'react';
+import { Button, Grid, Textarea, Input, Card, Text, Row  } from "@nextui-org/react";
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state={
+      descriptionError: "",
       notes:[]
     }
   }
@@ -15,18 +17,39 @@ class App extends Component {
     this.refreshNotes();
   }
 
-  async refreshNotes(){
+  clearInputFields() {
+    document.getElementById("title").value = "";
+    document.getElementById("description").value = "";
+  }
+  
+  handleDescriptionChange = (event) => {
+    const descriptionValue = event.target.value;
+    if (descriptionValue.length > 500) {
+      this.setState({ descriptionError: true });
+    } else {
+      this.setState({ descriptionError: false });
+    }
+  }
+  
+  async refreshNotes() {
     fetch(this.BASE_URL + "api/TodoApp/GetNotes")
     .then(response=>response.json())
     .then(data=>{
+      console.log(data);
       this.setState({notes:data});
+    })
+    .catch(error => {
+      alert("Error getting notes:" + error);
     })
   } 
 
   async addNote() {
-    var newNotes = document.getElementById("newNotes").value;
+    var title = document.getElementById("title").value;
+    var description = document.getElementById("description").value;
     const data = new FormData();
-    data.append("newNotes", newNotes);
+
+    data.append("title", title);
+    data.append("description", description)
 
     fetch(this.BASE_URL + "api/TodoApp/AddNotes", {
       method: "POST",
@@ -36,6 +59,10 @@ class App extends Component {
     .then((result)=> {
       alert(result);
       this.refreshNotes();
+      this.clearInputFields();
+    })
+    .catch(error => {
+      alert("Error adding note to the database:", error)
     })
   }
 
@@ -48,25 +75,119 @@ class App extends Component {
         alert(result);
         this.refreshNotes();
       })
+      .catch(error => {
+        alert("Error:" + error)
+      })
+
+  }
+  async deletAllNotes() {
+    fetch(this.BASE_URL + "api/TodoApp/DeleteAllNotes", {
+      method: "DELETE"
+    })
+      .then(() => {
+        alert("All notes deleted successfully");
+        this.refreshNotes(); // Update the notes state directly after successful deletion
+      })
+      .catch(error => {
+        alert("Error deleting notes: " + error);
+      });
   }
 
   render() {
     const{notes} = this.state;
      
     return (
-        <div className="App">
-          <h2>C# Dotnet Todo App</h2>
-          <input id="newNotes"/>&nbsp;
-          <button onClick={()=> this.addNote()}>Add Note</button>
-          {notes.map(note=>
-            <p>
-              <b>{note.description}</b>&nbsp;
-              <button onClick={() => this.deleteNote(note.id)}>Delete Note</button>
-            </p>
-          )}
+      <div className="App">
+
+        <h2>Web Notes</h2>
+
+        <div className="form-fields">
+          <Input className='title-input' id="title" placeholder="Title" />
+          
+          <Textarea
+            className='description-input'
+            id="description"
+            placeholder="Description"
+            cols="50"
+            rows="8" // Adjust the number of rows to set the height
+            onChange={this.handleDescriptionChange} // Call the function when input changes
+          />
+
+          <div className='add-delete-all-container'>
+            <Grid.Container gap={2}>
+              <Grid>
+                <Button color="success" auto onClick={() => this.addNote()}>Add</Button>
+              </Grid>
+              <Grid>
+                <Button color="warning" auto onClick={() => this.deletAllNotes()}>Delete All</Button>
+              </Grid>
+            </Grid.Container>
+          </div>
+          {this.state.descriptionError && <p className="error">Description cannot exceed 500 characters.</p>}
         </div>
-      )
+
+
+        <div className="note-list">
+          {notes.map(note =>
+            <div className="note-item-container" key={note.NoteId}>
+
+              {/* <Grid.Container gap={3}>
+                <Grid sm={2} md={12}>
+                  <Card css={{ mw: "330px" }}>
+                    <Card.Header>
+                      <h3 className='note-title'>{note.Title}</h3>
+                    </Card.Header>
+                    <Card.Divider />
+                    <Card.Body css={{ py: "$10" }}>
+                      <Text>
+                        {note.Description}
+                      </Text>
+                    </Card.Body>
+                    <Card.Divider />
+                    <Button color="warning" auto onClick={() => this.deleteNote(note.NoteId)}>Delete</Button>
+                  </Card>
+                </Grid>
+
+              </Grid.Container> */}
+
+              <Grid.Container gap={2}>
+                <Grid >
+                  <Card css={{ mw: "550px" }}>
+                    <Card.Header>
+                      <Text b>{note.Title}</Text>
+                    </Card.Header>
+                    <Card.Divider />
+                    <Card.Body css={{ py: "$10" }}>
+                      <p className='note-text'>{note.Description}</p>
+                    </Card.Body>
+                    <Card.Divider />
+                    <Card.Footer>
+                      <Row justify="flex-end" css={{marginRight: "25px"}}>
+                        <Button size="sm" light>Edit</Button>
+                        <Button size={"sm"} color="error" auto onClick={() => this.deleteNote(note.NoteId)}>Delete</Button> 
+                      </Row>
+                    </Card.Footer>
+                  </Card>
+                </Grid>
+              </Grid.Container>
+
+            </div>
+          ).reverse()}
+        </div>
+      </div>
+    );
   }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
